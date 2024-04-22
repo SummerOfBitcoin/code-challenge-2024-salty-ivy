974dcf412011729c76c67d87dc99f436edf144f5
Aman Pandey <aman2001mi@gmail.com>
2024-04-22 12:42:32 +0530

    optimize score more than 60

--- block.py
+++ block.py
@@ -299,7 +299,7 @@ def main():
     with open("valid-cache.json", "r") as file:
         unverified_txns = json.load(file)
 
-    for tx in unverified_txns[:2500]:
+    for tx in unverified_txns[:2100]:
         verified_tx = pre_process_transaction(tx)
         transactions.append(verified_tx)
 
